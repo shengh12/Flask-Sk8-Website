da22c34046c4283404ce86cf647f75670a03b0b8
Sheng Huang <shengh@mit.edu>
2021-12-28 16:46:51 -0500

    database first tutorial

--- flaskCode.py
+++ flaskCode.py
@@ -1,10 +1,36 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'ea109067810c1cb3a44106ece4e0d0b4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}')"
+    
+    
 skate_news_data = [
     {
         'title':'Skateboarding is Cool',
@@ -28,7 +54,7 @@ def home():
 
 @app.route("/news")
 def news():
-    return render_template('SK8News.html', posts=skate_news_data, title="Sk8 News")
+    return render_template('SK8News.html', posts=skate_news_data, title="Sk8 Posts")
 
 
 @app.route("/tricks")
