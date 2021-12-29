568bbbf4f9616d8980851d7d09fc411e46ecb01d
Sheng Huang <shengh@mit.edu>
2021-12-29 16:27:37 -0500

    profile pics

--- flaskCode/forms.py
+++ flaskCode/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskCode.models import User
-
 
 class RegistrationForm(FlaskForm):
   username = StringField('Username', 
@@ -34,3 +35,26 @@ class LoginForm(FlaskForm):
   password = PasswordField('Password', validators=[DataRequired()])
   remember = BooleanField('Remember Me')
   submit = SubmitField('Login')
+  
+class UpdateAccountForm(FlaskForm):
+  username = StringField('Username', 
+                         validators=[DataRequired(), Length(min=6, max=10)])
+  
+  email = StringField('Email',
+                      validators=[DataRequired(), Email()])
+  
+  picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+  
+  submit = SubmitField('Update')
+  
+  def validate_username(self, username):
+    if username.data != current_user.username:
+      user = User.query.filter_by(username=username.data).first()
+      if user:
+        raise ValidationError('Username Already Taken')
+  
+  def validate_email(self, email):
+    if email.data != current_user.email:
+      user = User.query.filter_by(email=email.data).first()
+      if user:
+        raise ValidationError('Email Already Taken')
